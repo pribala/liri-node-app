357ff8bf0066d253a9736d9a30e3f165d0d8518e
Priya Balakrishnan <andu_pri@yahoo.com>
2017-09-28 13:19:51 -0400

    feat: Initial setup files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'VEMsYhvdcdBQv2uBtgrNU8pBw',
+  consumer_secret: 'ZXPZS80jbCPktkR63h6kaQ1AxspaUFUqL2MBCEtZf8GRfVfFrk',
+  access_token_key: '826142425194311681-Pb0YeAfMChM3wBno2q0KdVJo0EB5Lqp',
+  access_token_secret: 'DeUSJSodgUVif3Q7Wsvacq9QUWz2l9ClRht05A2s7UvZ5',
+}
+
+module.exports = twitterKeys;
